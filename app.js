009dd04df0463b62e2211c291fbb93e1870ca58c
Roni769361 <60136750+Roni769361@users.noreply.github.com>
2022-08-15 18:27:11 +0600

    logingsystem

--- app.js
+++ app.js
@@ -5,3 +5,33 @@ function clickGallery(){
     document.getElementById("gallary").style.display='block';
     document.getElementById("galleryfit").style.display='block';
 }
+function home(){
+    document.getElementById("gallary").style.display = 'none';
+    document.getElementById("galleryfit").style.display = 'none';
+    document.getElementById("topHeader").style.display = 'block';
+
+}
+function logout(){
+    document.getElementById("gallary").style.display = 'none';
+    document.getElementById("galleryfit").style.display = 'none';
+    document.getElementById("HomeText").style.display = 'none';
+    document.getElementById("topHeader").style.display = 'block';
+    document.getElementById("loging").style.display = 'block';
+
+}
+
+function sinin(){
+
+    const email = document.getElementById("form2Example1").value;
+    const pass = document.getElementById("form2Example2").value;
+
+    if(email == "" && pass == ""){
+        document.getElementById("HomeText").style.display = 'block';
+        document.getElementById("loging").style.display = 'none';
+
+    }
+    else{
+        alert("Sorry, User ID or Pass Code Not Match. Please Try Again.")
+    }
+
+}
