a35176d3fb811d156b2c81e21d686599fd051b24
Roni769361 <60136750+Roni769361@users.noreply.github.com>
2022-08-15 11:07:16 +0600

    cssandquary

---
+++ app.js
@@ -0,0 +1,7 @@
+// document.getElementById("clickGallery").addEventListener("click",)
+
+function clickGallery(){
+    document.getElementById("topHeader").style.display='none';
+    document.getElementById("gallary").style.display='block';
+    document.getElementById("galleryfit").style.display='block';
+}
